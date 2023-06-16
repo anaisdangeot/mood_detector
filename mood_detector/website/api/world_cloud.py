dbb09014fbc1ab1268777b37424768664d251032
mathieu thiboudois <mattthiboudois@gmail.com>
2023-06-16 11:36:06 +0400

    good structure

--- mood_detector/website/api/world_cloud.py
+++ mood_detector/website/api/world_cloud.py
@@ -7,4 +7,3 @@ text = open('lyrics_parts_test.txt',mode="r",encoding='utf-8').read()
 wc = WordCloud(mode = "RGBA", background_color=None,  height=1600, width=1400)
 wc.generate(text)
 wc.to_file('gghj.png')
-...
